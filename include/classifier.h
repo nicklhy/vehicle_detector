06bbe5ec881107e2bcdaafda4d0ca7285b7e98c3
nicklhy <nicklhy@gmail.com>
2015-07-14 10:45:42 +0800

    Init project

---
+++ include/classifier.h
@@ -0,0 +1,58 @@
+#ifndef __CLASSIFIER_H__
+#define __CLASSIFIER_H__
+
+
+#include <caffe/caffe.hpp>
+#include <opencv2/opencv.hpp>
+#include <iosfwd>
+#include <memory>
+#include <string>
+#include <sstream>
+#include <utility>
+#include <vector>
+
+
+using namespace caffe;  // NOLINT(build/namespaces)
+using namespace std;
+
+
+/* Pair (label, confidence) representing a prediction. */
+typedef std::pair<string, float> Prediction;
+
+
+class Classifier {
+    public:
+        Classifier();
+        Classifier(const string& model_def,
+                const string& trained_weights,
+                const string& mean_file = "",
+                const string& label_file = "",
+                const int &gpu_id = 0);
+        void init(const string& model_def,
+                const string& trained_weights,
+                const string& mean_file = "",
+                const string& label_file = "",
+                const int &gpu_id = 0);
+
+        std::vector<Prediction> classify(const cv::Mat& img, int N = 5);
+
+    private:
+        void SetMean(const string& mean_file);
+
+        std::vector<float> Predict(const cv::Mat& img);
+
+        void WrapInputLayer(std::vector<cv::Mat>* input_channels);
+
+        void Preprocess(const cv::Mat& img,
+                std::vector<cv::Mat>* input_channels);
+
+    private:
+        /* data */
+        shared_ptr<Net<float> > net_;
+        cv::Size input_geometry_;
+        int num_channels_;
+        cv::Mat mean_;
+        std::vector<string> labels_;
+};
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "classifier.h"
+
+int main(int argc, char *argv[])
+{
+    Classifier classifier;
+    return 0;
+}
