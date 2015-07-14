acd7054e82bccc4bdc04214f83b51679e0930fc6
nicklhy <nicklhy@gmail.com>
2015-07-14 11:22:12 +0800

    update makefile

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include "classifier.h"
 
-int main(int argc, char *argv[])
-{
+using namespace std;
+
+int main(int argc, char *argv[]) {
     Classifier classifier;
     return 0;
 }
